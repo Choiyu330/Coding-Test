9697ad9bc40343aa446dbbabb388a6ca85958cd3
Yujin_Choi <jenny4063@naver.com>
2022-11-03 21:07:03 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+
+        for(int i=0; i<arr.length; i++) { arr[i] = sc.nextInt(); }
+
+        int max = arr[0];
+        int temp = 1;
+
+        for(int i=1; i<=arr.length; i++) {
+            if(max < arr[i-1]) {
+                max = arr[i-1];
+                temp = i;
+            }
+
+        }
+        System.out.println(max + "\n" + temp);
+    }
+}

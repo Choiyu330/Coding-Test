70d2e9f344d6ca5d6ffd76a958b936fed6274221
Yujin_Choi <jenny4063@naver.com>
2022-11-03 22:49:21 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int[] student = new int[31];
+        for(int i=1; i<=28; i++) {
+            student[sc.nextInt()]++;
+        }
+        for(int i=1; i<=30; i++) {
+            if(student[i] == 0) System.out.println(i);
+        }
+    }
+}

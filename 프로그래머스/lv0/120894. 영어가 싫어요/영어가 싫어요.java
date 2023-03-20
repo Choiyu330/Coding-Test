8870666cc92ed403ed988d3f76410f57dfa65533
Yujin_Choi <jenny4063@naver.com>
2023-03-20 13:41:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 2.22 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Solution {
+    public long solution(String numbers) {
+        String answer = "";
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(0, "zero");
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.put(6, "six");
+        map.put(7, "seven");
+        map.put(8, "eight");
+        map.put(9, "nine");
+
+        String str = "";
+        
+        for(int i=0; i<numbers.length(); i++) {
+            str += String.valueOf(numbers.charAt(i));
+            if (str.length() > 2) {
+                for(int j=0; j<=map.size(); j++) {
+                    if (str.equals(map.get(j))) {
+                        answer += String.valueOf(j);
+                        str = "";
+                        break;
+                    }
+                }
+            }
+        }
+        return Long.parseLong(answer);
+    }
+}

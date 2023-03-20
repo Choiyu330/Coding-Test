f17d3d67f10d308229960ef721ad33015b758e84
Yujin_Choi <jenny4063@naver.com>
2023-03-20 13:42:55 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.56 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -5,16 +5,11 @@ class Solution {
         String answer = "";
         HashMap<Integer, String> map = new HashMap<>();
 
-        map.put(0, "zero");
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-        map.put(8, "eight");
-        map.put(9, "nine");
+        map.put(0, "zero");     map.put(1, "one");
+        map.put(2, "two");      map.put(3, "three");
+        map.put(4, "four");     map.put(5, "five");
+        map.put(6, "six");      map.put(7, "seven");
+        map.put(8, "eight");    map.put(9, "nine");
 
         String str = "";
         

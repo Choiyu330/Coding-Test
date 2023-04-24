bcc49706b13600f6f995bc8861977118a80dfe20
Yujin_Choi <jenny4063@naver.com>
2023-04-25 03:10:09 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -3,17 +3,9 @@ class Solution {
         int answer = 0;
         int sqrt = (int)Math.sqrt(n);
         
-        for(int i=1; i<=sqrt; i++) {
-            // 약수 중 작은 수 구하기
-            // ex) 10의 약수 2,5 중 2
-            if(n % i == 0) {
+        for(int i = 1; i <= n; i++){
+	        if(n % i == 0){
                 answer += i;
-                // 약수 중복 방지
-                if(n / i != i) { 
-                    // 약수 중 큰 수 구하기
-                    // ex) 10의 약수 2,5 중 5
-                    answer += n / i;
-                }
             }
         }
         return answer;

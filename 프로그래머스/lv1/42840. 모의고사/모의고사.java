e5f87b0bb010070232fc7829c3de007a6a312e54
Yujin_Choi <jenny4063@naver.com>
2023-04-30 01:57:22 +0900

    [level 1] Title: 모의고사, Time: 0.61 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -8,21 +8,18 @@ class Solution {
         int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         for(int i=0; i<answers.length; i++) {
-            if(answers[i] == a[i % a.length])
-                score[0]++;
-            if(answers[i] == b[i % b.length])
-                score[1]++;
-            if(answers[i] == c[i % c.length])
-                score[2]++;
+            if(answers[i] == a[i % a.length]) score[0]++;
+            if(answers[i] == b[i % b.length]) score[1]++;
+            if(answers[i] == c[i % c.length]) score[2]++;
         }
         
         int max = Math.max(score[0], Math.max(score[1], score[2]));
       
         ArrayList<Integer> arr = new ArrayList<>();
         
-        if(max == score[0]) { arr.add(1); }
-        if(max == score[1]) { arr.add(2); }
-        if(max == score[2]) { arr.add(3); }
+        if(max == score[0]) arr.add(1);
+        if(max == score[1]) arr.add(2);
+        if(max == score[2]) arr.add(3);
 
         int[] answer = new int[arr.size()];
         

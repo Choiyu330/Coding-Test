d1b29fb152ad3fdf7908d7fcc9c8a2866a5fbe11
Yujin_Choi <jenny4063@naver.com>
2023-06-01 20:42:58 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 66.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        System.out.println(answer.length);
+        
+        for(int i=0; i<commands.length; i++) {
+            int[] num_arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(num_arr);
+            answer[i] = num_arr[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}

3d813fbf74f0bd2961e059a7e931ea15ee1dc050
Yujin_Choi <jenny4063@naver.com>
2023-08-04 18:35:11 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.51 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,22 +1,21 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        Character[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int cnt = 0;
         
-        for(int i=0; i<strings.length-1; i++){
-          for(int k=i+1; k<strings.length;k++){
-              if(strings[i].charAt(n)>strings[k].charAt(n)){ 
-                  String tmp = strings[i];
-                  strings[i]= strings[k];
-                  strings[k]=tmp;
-              }
-              else if(strings[i].charAt(n)==strings[k].charAt(n)){
-                  if(strings[i].compareTo(strings[k]) >0){ 
-                      String tmp = strings[i];
-                      strings[i]= strings[k];
-                      strings[k]=tmp;
-                  }
-              }
-          }
-      }
-      return strings;
+        Arrays.sort(strings);
+        
+        for(int i=0; i<alphabet.length; i++) {
+            for(int j=0; j<strings.length; j++) {
+                if(strings[j].charAt(n) == alphabet[i]) {
+                    answer[cnt] = strings[j];
+                    cnt ++;
+                }  
+            }
+        }
+        return answer;
     }
 }

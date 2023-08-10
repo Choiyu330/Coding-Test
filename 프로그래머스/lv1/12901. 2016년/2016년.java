38e0068881f1b521f8ab9180f2e28c6900347124
Yujin_Choi <jenny4063@naver.com>
2023-08-10 19:23:34 +0900

    [level 1] Title: 2016년, Time: 1.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,10 +1,7 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.time.*;
 
 class Solution {
     public String solution(int a, int b) {    
-        return LocalDate.of(2016, a, b).getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }

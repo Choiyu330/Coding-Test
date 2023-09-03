85f2401de347e0387823653f91ab9fd646dfbb6c
Yujin_Choi <jenny4063@naver.com>
2023-09-03 22:44:32 +0900

    [unrated] Title: 세 개의 구분자, Time: 42.02 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,30 +1,8 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public String[] solution(String myStr) {
-        ArrayList<String> strList = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder();
-        char[] arr = myStr.toCharArray();
-        
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 'a' || arr[i] == 'b' || arr[i] == 'c') {
-                if(sb.length() != 0) {    
-                    strList.add(sb.toString());
-                    sb.setLength(0);
-                }
-            } else if (arr[i] != 'a' || arr[i] != 'b' || arr[i] != 'c') {
-                sb.append(arr[i]);
-                if(i==arr.length-1) {
-                    strList.add(sb.toString());
-                    sb.setLength(0);
-                }
-            }
-        }
-        
-        if(strList.size() == 0) {
-            return new String[] {"EMPTY"};
-        }
-        
-        return strList.toArray(new String[strList.size()]);
+        String[] answer = Arrays.stream(myStr.split("[abc+]")).filter(i -> !i.isEmpty()).toArray(String[]::new);
+        return answer.length == 0 ? new String[]{"EMPTY"} : answer;
     }
 }

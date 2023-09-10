fa4730e7583115e5cf3d594ea7733ded63f92e5a
Yujin_Choi <jenny4063@naver.com>
2023-09-10 23:38:19 +0900

    [unrated] Title: 2의 영역, Time: 1.27 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 2) {
+                if(start == -1) start = i;
+                else end = i;
+            }
+        }
+        
+        if(start == -1)
+            answer = new int[]{-1};
+        else if(start != -1 && end == -1)
+            answer = new int[]{2};
+        else
+            answer = Arrays.copyOfRange(arr, start, end + 1);
+        
+        return answer;
+    }
+}

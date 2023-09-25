c2bbbcd95bd09011d5a056b047c203e37a5ebcc8
Yujin_Choi <jenny4063@naver.com>
2023-09-25 23:48:09 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.30 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,22 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[][] solution(int[][] arr) {
-        int oneLen = arr.length;
-        int twoLen = arr[0].length;
-        int[][] answer1 = new int[oneLen][oneLen];
-        int[][] answer2 = new int[twoLen][twoLen];
-        if(oneLen > twoLen) {
-            for(int i =0; i<oneLen; i++) {
-                answer1[i] = Arrays.copyOf(arr[i], oneLen);
+        int len = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[len][len];
+        
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[0].length; j++) {
+                answer[i][j] = arr[i][j];
             }
-            return answer1;
-        }else if(twoLen > oneLen) {
-            for(int i =0; i<oneLen; i++) {
-                answer2[i] = Arrays.copyOf(arr[i], twoLen);
-            }
-            return answer2;
         }
-        return arr;
+        return answer;
     }
 }

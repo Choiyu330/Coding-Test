e175c38d93af0efec6864b09d019d5601fcb184a
Yujin_Choi <jenny4063@naver.com>
2023-10-04 23:50:23 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 15.55 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        return my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length(), my_string.length()); 
-    }
-}
+        return my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length(), my_string.length());
+    } 
+} 
